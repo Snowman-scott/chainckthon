edd66242085fc2895cd0313677ea9231f2f33222
Rose <snowm4808@gmail.com>
2025-10-26 04:23:28 +0000

    added starting blocks again

--- messaging.py
+++ messaging.py
@@ -1,2 +1,10 @@
 from encryption import encrypt_message, decrypt_message
 from message_storage import save_message, get_messages_for_user
+
+def send_message_ui(sessiom):
+    # Send message ui/function
+    pass # CODE THIS CHILD
+
+def check_inbox_ui(session):
+    # UI/ Function for checking messages
+    pass # CODE THIS CHILD
